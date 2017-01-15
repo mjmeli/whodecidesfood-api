a7d9f0a306497e62f2398019fc8e86a07c154130
Michael Meli <mjmeli94@gmail.com>
2017-01-14 19:46:48 -0500

    on application exception, return error message as json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  rescue_from StandardError do |exception|
+    raise unless request.format.json?
+    render json: { :error => exception.message }, :status => 500
+  end
 end
