b1c954825a235813dce83fb1b07310db8c8ac32d
Michael Meli <mjmeli94@gmail.com>
2017-01-14 19:09:16 -0500

    fixes show action on users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
   respond_to :json
 
   def show
-    respond_with User.find(params[:id])
+    respond_with User.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   # API definition
-  namespace :api, defaults: { format: :json }, path: '/' do
+  namespace :api, defaults: { format: :json }, path: '/api/' do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
       # List resources below
