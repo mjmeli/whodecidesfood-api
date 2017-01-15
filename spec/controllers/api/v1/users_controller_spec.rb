aeae44acbfd50c92b7a0478e34f9c7dd4e231a80
Michael Meli <mjmeli94@gmail.com>
2017-01-15 00:35:01 -0500

    add authorization for the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -54,9 +54,13 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe "PUT/PATCH #update" do
 
+    before(:each) do
+      @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token
+    end
+
     context "when is successfully updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
         patch :update, params: { id: @user.id,
                                  user: { email: "newmail@example.com" } }
       end
@@ -71,7 +75,6 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
     context "when is not created" do
       before(:each) do
-        @user = FactoryGirl.create :user
         patch :update, params: { id: @user.id, user: { email: "bademail.com" } }
       end
 
@@ -92,6 +95,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
+      api_authorization_header @user.auth_token
       delete :destroy, params: { id: @user.id }
     end
 
--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -19,5 +19,9 @@ module Request
       api_header
       api_response_format
     end
+
+    def api_authorization_header(token)
+      request.headers['Authorization'] = token
+    end
   end
 end
