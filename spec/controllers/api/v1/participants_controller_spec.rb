02b3ff4c7d17d5efb3173c5012a211155c4ae3a5
Michael Meli <mjmeli94@gmail.com>
2017-01-29 17:38:52 -0500

    add tests for participant creation failure

--- spec/controllers/api/v1/participants_controller_spec.rb
+++ spec/controllers/api/v1/participants_controller_spec.rb
@@ -37,19 +37,41 @@ RSpec.describe Api::V1::ParticipantsController, type: :controller do
 
   # CREATE
   describe "POST #create" do
-    before(:each) do
-      comparison = FactoryGirl.create :comparison
-      participant_params = { name: "Fake Name" }
-      post :create, params: { comparison_id: comparison.id, participant: participant_params }
+    context "when is successfully created" do
+      before(:each) do
+        comparison = FactoryGirl.create :comparison
+        participant_params = { name: "Fake Name" }
+        post :create, params: { comparison_id: comparison.id, participant: participant_params }
+      end
+
+      it "returns the just participant record" do
+        participants_response = json_response
+        expect(participants_response[:id]).to be_present
+        expect(participants_response[:name]).to eql("Fake Name")
+        expect(participants_response[:score]).to eql(0)
+      end
+
+      it { should respond_with 201 }
     end
 
-    it "returns the just participant record" do
-      participants_response = json_response
-      expect(participants_response[:id]).to be_present
-      expect(participants_response[:name]).to eql("Fake Name")
-      expect(participants_response[:score]).to eql(0)
-    end
+    context "when is not successfully created" do
+      before(:each) do
+        comparison = FactoryGirl.create :comparison
+        participant_params = { name: "" }
+        post :create, params: { comparison_id: comparison.id, participant: participant_params }
+      end
 
-    it { should respond_with 201 }
+      it "renders an errors json" do
+        participants_response = json_response
+        expect(participants_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the comparison could not be created" do
+        participants_response = json_response
+        expect(participants_response[:errors][:name]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
   end
 end
