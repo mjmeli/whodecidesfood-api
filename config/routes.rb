c82305fec9d1330166c03d7189f5246a3695c80d
Michael Meli <mjmeli94@gmail.com>
2017-01-29 16:34:26 -0500

    reserve space for participants resource under comparisons

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
       # List resources below
       resources :users, :only => [:show, :create, :update, :destroy]
       resources :sessions, :only => [:create, :destroy]
-      resources :comparisons, :only => [:show, :index, :create, :update, :destroy]
+      resources :comparisons, :only => [:show, :index, :create, :update, :destroy] do
+        # participants resources will go here
+      end
     end
   end
 end
