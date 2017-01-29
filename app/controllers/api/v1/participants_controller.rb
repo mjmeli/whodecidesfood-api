5305c8720f73d16f3735872af88cbc4eee140c49
Michael Meli <mjmeli94@gmail.com>
2017-01-29 17:18:20 -0500

    add show action for participants

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -3,6 +3,16 @@ class Api::V1::ParticipantsController < ApplicationController
 
   def index
     # Returns all participants under the comparison id
-    respond_with Comparison.find(params[:comparison_id]).participants
+    respond_with current_comparison.participants
   end
+
+  def show
+    respond_with current_comparison.participants.find(params[:id])
+  end
+
+  private
+
+    def current_comparison
+      @current_comparison ||= Comparison.find(params[:comparison_id])
+    end
 end
--- spec/controllers/api/v1/participants_controller_spec.rb
+++ spec/controllers/api/v1/participants_controller_spec.rb
@@ -5,16 +5,33 @@ RSpec.describe Api::V1::ParticipantsController, type: :controller do
   describe "GET #index" do
     before(:each) do
       comparison = FactoryGirl.create :comparison
-      4.times { FactoryGirl.create :participant, comparison: comparison }
+      6.times { FactoryGirl.create :participant, comparison: comparison }
       get :index, params: { comparison_id: comparison.id }
     end
 
     it "returns 4 participants from the comparison" do
       participants_response = json_response
-      expect(participants_response.length).to eql(4)
+      expect(participants_response.length).to eql(6)
     end
 
     it { should respond_with 200 }
   end
 
+  # SHOW
+  describe "GET #show" do
+    before(:each) do
+      comparison = FactoryGirl.create :comparison
+      4.times { FactoryGirl.create :participant, comparison: comparison }
+      @participant = FactoryGirl.create :participant, comparison: comparison
+      get :show, params: { comparison_id: comparison.id, id: @participant.id }
+    end
+
+    it "returns the participant record matching the id" do
+      participants_response = json_response
+      expect(participants_response[:id]).to eql @participant.id
+      expect(participants_response[:name]).to eql @participant.name
+    end
+
+    it { should respond_with 200 }
+  end
 end
