c5740a4e500ecc3c5e6eb79ecc89c03549e909c0
Michael Meli <mjmeli94@gmail.com>
2017-01-29 18:46:37 -0500

    require authentication to view user on production only

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_with_token!, only: [:update, :destroy]
+  before_action :authenticate_with_token!, only: [:show], if: :production?
   respond_to :json
 
   def show
@@ -33,5 +34,9 @@ class Api::V1::UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+    def production?
+      Rails.env == "production"
     end
 end
