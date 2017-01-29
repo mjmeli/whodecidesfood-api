c2b5dca892e7e9eb91630f6e61d8af8c717667ec
Michael Meli <mjmeli94@gmail.com>
2017-01-29 16:33:52 -0500

    moved comparisons out of users route

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/api/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # List resources below
-      resources :users, :only => [:show, :create, :update, :destroy] do
-        resources :comparisons, :only => [:create, :update, :destroy]
-      end
+      resources :users, :only => [:show, :create, :update, :destroy]
       resources :sessions, :only => [:create, :destroy]
-      resources :comparisons, :only => [:show, :index]
+      resources :comparisons, :only => [:show, :index, :create, :update, :destroy]
     end
   end
 end
