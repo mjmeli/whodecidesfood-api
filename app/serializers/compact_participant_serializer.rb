4bc2e3f3f87ffd8daf13c980b3654ef2fd9c1222
Michael Meli <mjmeli94@gmail.com>
2017-01-31 19:31:42 -0500

    added compact participants serializer for user within comparisons

---
+++ app/serializers/compact_participant_serializer.rb
@@ -0,0 +1,3 @@
+class CompactParticipantSerializer < ActiveModel::Serializer
+  attributes :id, :name, :score
+end
--- app/serializers/comparison_serializer.rb
+++ app/serializers/comparison_serializer.rb
@@ -1,6 +1,6 @@
 class ComparisonSerializer < ActiveModel::Serializer
   attributes :id, :title, :owner
-  has_many :participants
+  has_many :participants, serializer: CompactParticipantSerializer
   attribute :decision_ids
 
   # Put the owner in the json response, but use the compact owner serializer
