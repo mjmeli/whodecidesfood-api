87dbcebcf665139d46955ccb7ca11397c87065bb
Michael Meli <mjmeli94@gmail.com>
2017-02-06 17:51:51 -0500

    added a known test user to the database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def create_comparisons_for_user(user)
+  3.times do
+    comparison = FactoryGirl.create :comparison, owner: user
+    2.times do
+      participant = FactoryGirl.create :participant, comparison: comparison
+      5.times { FactoryGirl.create :decision, comparison: comparison, participant: participant }
+    end
+  end
+end
+
 case Rails.env
 when "development"
   # Users with comparisons
@@ -12,15 +22,13 @@ when "development"
     user = FactoryGirl.create :user
 
     # Create comparisons with participants
-    3.times do
-      comparison = FactoryGirl.create :comparison, owner: user
-      2.times do
-        participant = FactoryGirl.create :participant, comparison: comparison
-        5.times { FactoryGirl.create :decision, comparison: comparison, participant: participant }
-      end
-    end
+    create_comparisons_for_user(user)
   end
 
   # Users with no comparisons
   3.times { FactoryGirl.create :user }
+
+  # Known user
+  user = FactoryGirl.create :user, email: "foo@bar.com", password: "test123", password_confirmation: "test123"
+  create_comparisons_for_user(user)
 end
