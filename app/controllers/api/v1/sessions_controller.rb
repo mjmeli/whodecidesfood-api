3cab1d43d1e9ca095a920840f7def977ce734ffc
Michael Meli <mjmeli94@gmail.com>
2017-02-06 14:52:40 -0500

    validate user exists before checking the user password when creating sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::SessionsController < ApplicationController
     user_email = params[:session][:email]
     user = user_email.present? && User.find_by(email: user_email)
 
-    if user.valid_password? user_password
+    if user && user.valid_password?(user_password)
       sign_in user, store: false
       user.generate_authentication_token!
       user.save
