d03208ce7493ed9d6b796c0d73ab09063f07e334
Michael Meli <mjmeli94@gmail.com>
2017-02-12 11:51:42 -0500

    added ordering of participants in comparison results by created date

--- app/serializers/comparison_serializer.rb
+++ app/serializers/comparison_serializer.rb
@@ -1,11 +1,19 @@
 class ComparisonSerializer < ActiveModel::Serializer
   attributes :id, :title, :owner
-  has_many :participants, serializer: CompactParticipantSerializer
+  # has_many :participants, serializer: CompactParticipantSerializer
+  attributes :participants
   attribute :decision_ids
 
   # Put the owner in the json response, but use the compact owner serializer
   def owner
     CompactUserSerializer.new(object.owner)
+  end
+
+  # Order participants by created date
+  def participants
+    object.participants.order("created_at").map{ |p|
+      CompactParticipantSerializer.new(p)
+    }
   end
 
   # has_one :owner # put the owner in the json response for now
