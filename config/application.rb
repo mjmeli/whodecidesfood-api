765bbdce3dd1b443524e95f72f963ea3bcdc0db9
Michael Meli <mjmeli94@gmail.com>
2017-09-01 20:50:03 -0400

    CORS for PUT

--- config/application.rb
+++ config/application.rb
@@ -41,7 +41,7 @@ module WhodecidesfoodApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
   end
